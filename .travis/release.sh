1f9d804a2eee8d4ad7fe0051cee51832e428d20f
Rafał Wrzeszcz <rafal.wrzeszcz@wrzasq.pl>
2017-08-12 02:27:47 +0200

    CD pipeline - next attempt.

--- .travis/release.sh
+++ .travis/release.sh
@@ -9,23 +9,21 @@
 
 set -ex
 
-BRANCH="${1}"
-
 REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 
 # change origin URL to SSH to allow uploading with SSH key
 git remote rm origin
-git remote add orogin ${SSH_REPO}
+git remote add origin ${SSH_REPO}
 
 # make sure HEAD points to the branch
-git checkout ${BRANCH}
+git checkout ${TRAVIS_BRANCH}
 
 # first make current version release
 mvn versions:set versions:commit -DremoveSnapshot=true
 git add -u
 git commit -m "Automated release release."
-git push origin ${BRANCH}:master
+git push origin ${TRAVIS_BRANCH}:master
 
 # now create a new version commit
 mvn versions:set versions:commit \
@@ -33,4 +31,4 @@ mvn versions:set versions:commit \
     -DnextSnapshot=true
 git add -u
 git commit -m "[skip ci] New version bump."
-git push
+git push origin ${TRAVIS_BRANCH}
